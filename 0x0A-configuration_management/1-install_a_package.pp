07086715f0ddce0dcb8d5a09901fa9eb6b7a7611
omar546 <omer.mkawy.om@gmail.com>
2024-02-03 02:54:45 +0200

    1 fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#!/usr/bin/pup
-# installes flask from pip
-package {'flask':
+#!/usr/bin/puppet
+# installs Flask from pip
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
