10218d049a0d86bf754ec867466fb5d95328da58
omar546 <omer.mkawy.om@gmail.com>
2024-02-03 03:37:23 +0200

    F

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -8,13 +8,13 @@ package { 'python3-pip':
   require => Package['python3.8'],
 }
 
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
 }
 
-package { 'werkzeug':
+package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
   require  => Package['python3-pip'],
