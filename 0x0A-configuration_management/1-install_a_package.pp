6c54b01fa2152b6744409675075598445eb1d95d
omar546 <omer.mkawy.om@gmail.com>
2024-02-03 03:23:24 +0200

    1.2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,9 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+# Install Werkzeug as a requirement for Flask
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
+}
