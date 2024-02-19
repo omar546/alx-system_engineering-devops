e6cd4d93356cfbd1063ac06a9908583cfd324bfb
omar546 <omer.mkawy.om@gmail.com>
2024-02-19 20:33:40 +0200

    styl

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,19 +5,17 @@ import requests
 import sys
 import urllib3
 
-
-
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id), verify=False).json()
+    user = requests.get(url + "users/{}".format(user_id),
+                        verify=False).json()
     username = user.get("username")
-    todoz = requests.get(url + "todos", params={"userId": user_id}, verify=False).json()
+    todoz = requests.get(url + "todos", params={"userId": user_id},
+                         verify=False).json()
 
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
             [user_id, username, tod.get("completed"), tod.get("title")]
          ) for tod in todoz]
-
-        
