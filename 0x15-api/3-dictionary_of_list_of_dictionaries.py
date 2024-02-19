34ee5c21789b7f736c1866f9dda33e93180ea0f4
omar546 <omer.mkawy.om@gmail.com>
2024-02-19 20:43:26 +0200

    styl

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,10 +8,17 @@ if __name__ == "__main__":
     users = requests.get(url + "users").json()
 
     with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            us.get("id"): [{
-                "task": tod.get("title"),
-                "completed": tod.get("completed"),
-                "username": us.get("username")
-            } for tod in requests.get(url + "todos", params={"userId": us.get("id")}).json()]
-            for us in users}, jsonfile)
+        json.dump(
+            {
+                us.get("id"): [
+                    {
+                        "task": tod.get("title"),
+                        "completed": tod.get("completed"),
+                        "username": us.get("username"),
+                    }
+                    for tod in requests.get(url + "todos", params={"userId": us.get("id")}).json()
+                ]
+                for us in users
+            },
+            jsonfile,
+        )
