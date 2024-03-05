25115899a66cd5335e111c794962e7ba373fea36
omar546 <omer.mkawy.om@gmail.com>
2024-03-05 22:04:01 +0200

    22

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-"""parsing web data from an api"""
-import json
+""" top ten from given page"""
 import requests
-import sys
 
 
-def recurse(subreddit, hot_list=[]):
-    """returns a list of the titles of all hot articles"""
-    reddit_url = "https://www.reddit.com/r/" + subreddit
-    reddit_url2 = reddit_url + "/hot.json?limit=100&after=after"
-    header = {"User-Agent": "Agent-Ano"}
-    req = requests.get(reddit_url2, headers=header)
-    reddit = req.json()
-
-    if (req.status_code == 200):
-        """checks if the response status is ok"""
-        hot_post = reddit.get("data").get("children")
-        after = reddit.get("data").get("after")
-        for posts in hot_post:
-            hot_list.append(posts.get("data").get("title"))
-        if after is None:
-            recurse(subreddit, hot_list, after)
-        return hot_list
-    else:
+def recurse(subreddit, hot_list=[], after=""):
+    """ return top ten"""
+    if not subreddit:
         return None
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "Ano"
+    }
+    params = {
+        "after": after
+    }
+    data = requests.get(url, headers=headers, params=params,
+                        allow_redirects=False).json()
+    if data == {'message': 'Not Found', 'error': 404}:
+        return None
+    info = data.get('data', {}).get('children', [])
+    after = data.get('data', {}).get('after', None)
+    for dic in info:
+        t = dic.get('data', {}).get('title', None)
+        if t:
+            hot_list.append(t)
+    if after is not None:
+        return recurse(subreddit, hot_list, after)
+    return hot_list
