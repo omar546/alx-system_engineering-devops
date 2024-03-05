1c2efa8f4d533edb5b7224520ed23b17c00f46c7
omar546 <omer.mkawy.om@gmail.com>
2024-03-05 21:38:21 +0200

    00

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,11 @@ import sys
 def number_of_subscribers(subreddit):
     """get num of subs"""
     base_url = 'https://www.reddit.com/r/'
-    headers = {'User-Agent': 'Agent-Ano'}
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
     url = base_url + '{}/about.json'.format(subreddit)
     response = requests.get(url, headers=headers)
     resp = json.loads(response.text)
