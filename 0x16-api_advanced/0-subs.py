8d229116bf6e5f9e94acb77fc6346736a52d7f63
omar546 <omer.mkawy.om@gmail.com>
2024-03-15 20:20:23 +0200

    0000

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""parsing data from api"""
-import json
+"""Gets subscriber count for subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """query the Reddit API"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Ano Agent 1.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
-    else:
-        return 0
+	headers = {'User-Agent': 'Ano/1.0'}
+	url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+	response = requests.get(url, headers=headers, allow_redirects=False)
+	if response.status_code != 200:
+		return 0
+
+	data = response.json()
+	if data["kind"] != "t5":
+		return 0
+	return data["data"]["subscribers"]
